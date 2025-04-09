083735276c5886619a9eff891f0c51ba222fa500
eckhell <rymjimenez333@gmail.com>
2025-04-09 22:44:56 +0200

    feat: primer comit
    feat: segundo commit tactico (bombardeen git)

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() { 
+  return (
+    <div>
+      <h1>¡Bienvenido a Nexa!</h1>
+      <p>Esta es una aplicación web desarrollada con React y Vite.</p>
+    </div>
+  )
+}
+
+export default App
